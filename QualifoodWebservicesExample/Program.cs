f73c2bc616888f7f5425a79a2171a955405a5611
Thomas Schreiner <thomas.schreiner@fleischpruefring.de>
2018-03-21 10:50:02 +0100

    Initial Checkin

--- QualifoodWebservicesExample/Program.cs
+++ QualifoodWebservicesExample/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using QualifoodWebservicesExample.Qualifood.Webservices;
 
 namespace QualifoodWebservicesExample
 {
     class Program
     {
+        private const string QualifoodBenutzername = "";
+        private const string QualifoodPassword = "";
+        private static readonly DateTime AbgefragtesSchlachtdatum = DateTime.Today.AddDays(-1);
+
         static void Main(string[] args)
         {
+            try
+            {
+                var client = CreateClient();
+
+                var result = ExecuteRequest(client);
+
+                PrintResult(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+
+        private static DatenDownloadClient CreateClient()
+        {
+            DatenDownloadClient client = new DatenDownloadClient("BasicHttpBinding_IDatenDownload");
+
+            client.ClientCredentials.UserName.UserName = QualifoodBenutzername;
+            client.ClientCredentials.UserName.Password = QualifoodPassword;
+            return client;
+        }
+
+        private static SchlachtdatenGV ExecuteRequest(DatenDownloadClient client)
+        {
+            var result = client.GetDatenDownloadGV(AbgefragtesSchlachtdatum);
+            return result;
+        }
+
+        private static void PrintResult(SchlachtdatenGV result)
+        {
+            Console.WriteLine($"Abgefragtes Schlachtdatum: {AbgefragtesSchlachtdatum:d}");
+            Console.WriteLine($"Anzahl Schlachtkörper: {result.Schlachtkoerper.Length}");
+            Console.WriteLine($"Anzahl Stammdaten: {result.Stammdaten.Length}");
         }
     }
 }
